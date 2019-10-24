a972120e47833f52b68e91702fa1b597242db2be
Javier Gonzalez <javi.gonp@gmail.com>
2019-10-23 23:04:59 -0300

    Added default format json

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'squads/index'
   #
   # get 'squads/show'
-  resources :squads, only: %i[index show]
+  resources :squads, only: %i[index show], defaults: { format: :json }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
