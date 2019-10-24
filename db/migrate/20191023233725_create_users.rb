ccff1e09a1b12196e6531ff1615635f1cff720e6
Javier Gonzalez <javi.gonp@gmail.com>
2019-10-23 22:11:33 -0300

    Added default values to user attributes

--- db/migrate/20191023233725_create_users.rb
+++ db/migrate/20191023233725_create_users.rb
@@ -2,11 +2,11 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :email, null: false
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :location
-      t.integer :position
-      t.string :slack_id
+      t.integer :position, null: true, default: nil
+      t.string :slack_id, null: true, default: nil
       t.timestamps
     end
   end
